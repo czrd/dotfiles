ab9bd11636693f7152b6008d1717e6f4de8b9120
Takeshi Itami <tksh.itm@gmail.com>
2022-08-17 23:58:59 +0900

    show hidden files in vimgrep

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -7,5 +7,16 @@ end
 telescope.setup {
   defaults = {
     winblend = 20,
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--smart-case",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--hidden",
+      "--glob=!.git/",
+    },
   },
 }
