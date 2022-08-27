7d9140e77279e5b587025ada9fd3e183caf11665
Takeshi Itami <tksh.itm@gmail.com>
2022-08-27 19:30:35 +0900

    fix symbols-outline config

--- nvim/.config/nvim/lua/config/symbols-outline.lua
+++ nvim/.config/nvim/lua/config/symbols-outline.lua
@@ -1,5 +1,11 @@
-vim.g.symbols_outline = {
-  width = 50,
+local ok, symbols_outline = pcall(require, "symbols-outline")
+if not ok then
+  print "symbols-outline is not installed"
+  return
+end
+
+symbols_outline.setup {
+  width = 30,
   show_symbol_details = false,
-  auto_preview = false,
+  auto_close = true,
 }
