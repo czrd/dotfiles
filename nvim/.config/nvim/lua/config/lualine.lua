05eedb63df3df33717944a76a0efa491ec3b7d76
Takeshi Itami <tksh.itm@gmail.com>
2022-08-29 20:53:42 +0900

    add lualine theme

--- nvim/.config/nvim/lua/config/lualine.lua
+++ nvim/.config/nvim/lua/config/lualine.lua
@@ -4,4 +4,39 @@ if not ok then
   return
 end
 
-ll.setup()
+local colors = {
+  fg = "#d4d4d4",
+  bg = "#007fd4",
+  gray = "#2c323d",
+  normal = "#007fd4",
+  insert = "#E53935",
+  visual = "#ffcf1f",
+  command = "#388E3C",
+}
+
+ll.setup {
+  options = {
+    theme = {
+      normal = {
+        a = { fg = colors.gray, bg = colors.normal },
+        b = { fg = colors.fg, bg = colors.gray },
+        c = { fg = colors.fg, bg = colors.gray },
+      },
+      insert = {
+        a = { fg = colors.gray, bg = colors.insert },
+      },
+      visual = {
+        a = { fg = colors.gray, bg = colors.visual },
+      },
+      command = {
+        a = { fg = colors.gray, bg = colors.command },
+      },
+      replace = {
+        a = { fg = colors.gray, bg = colors.bg },
+      },
+      inactive = {
+        a = { fg = colors.gray, bg = colors.normal },
+      },
+    },
+  },
+}
