8949fdd26f0e3aae9210adba51735e77efabf24b
Takeshi Itami <tksh.itm@gmail.com>
2022-08-31 01:09:29 +0900

    delete cursorlineopt

--- nvim/.config/nvim/lua/config/general.lua
+++ nvim/.config/nvim/lua/config/general.lua
@@ -13,7 +13,6 @@ vim.opt.expandtab = true -- convert tabs to spaces
 vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 2 -- insert 2 spaces for a tab
 vim.opt.cursorline = true -- highlight the current line
-vim.opt.cursorlineopt = "number"
 vim.opt.number = true -- set numbered lines
 vim.opt.relativenumber = true -- set relative numbered lines
 vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
