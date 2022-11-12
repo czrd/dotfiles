1ed6227411abe1566bfcbb975776248f4b7efc03
Takeshi Itami <tksh.itm@gmail.com>
2022-11-12 15:30:22 +0900

    adjust scroll speed

--- nvim/.config/nvim/lua/config/neoscroll.lua
+++ nvim/.config/nvim/lua/config/neoscroll.lua
@@ -12,10 +12,8 @@ neoscroll.setup {
 }
 
 require("neoscroll.config").set_mappings {
-  ["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "100" } },
-  ["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "100" } },
-  ["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "100" } },
-  ["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "100" } },
+  ["<C-b>"] = { "scroll", { "-vim.wo.scroll", "true", "100" } },
+  ["<C-f>"] = { "scroll", { "vim.wo.scroll", "true", "100" } },
   ["<C-y>"] = { "scroll", { "-0.10", "false", "100" } },
   ["<C-e>"] = { "scroll", { "0.10", "false", "100" } },
   ["zt"] = { "zt", { "100" } },
