4acb892368152f8b1fb53aa1ff1c2065a10153e6
Takeshi Itami <tksh.itm@gmail.com>
2023-02-12 22:18:58 +0900

    add fzf.zsh

---
+++ zsh/.config/zsh/fzf.zsh
@@ -0,0 +1,4 @@
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
+" --color=bg+:#e1e2e7,bg:#e1e2e7,spinner:#e1e2e7,hl:#e1e2e7"\
+" --color=fg:#414868,header:#2e7de9,info:#2e7de9,pointer:#f44747"\
+" --color=marker:#f44747,fg+:#f44747,prompt:#2e7de9,hl+:#f44747"
