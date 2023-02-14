dbb6a5a1008a97a7422766c27aba7afbe3124a0d
Takeshi Itami <tksh.itm@gmail.com>
2023-02-14 20:59:52 +0900

    change prompt style

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -1,4 +1,3 @@
 NEWLINE=$'\n'
-DIR='%B%K{#c4c8da}%F{#8990b3}  %2d  %f%k%b'
-PROMPT='%F{#c4c8da}%K{blue} $ %k%f'
-export PS1="${NEWLINE}${DIR}${PROMPT} "
+PROMPT='%F{#ffffff}%K{#febd69}  $  %k%f'
+export PS1="${NEWLINE}${PROMPT} "
