53ca1a10ae13a379eb8621f6e1b141d43f407cce
Takeshi Itami <tksh.itm@gmail.com>
2023-02-14 22:44:17 +0900

    fix fzf color

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -1,4 +1,4 @@
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
-" --color=bg+:#e1e2e7,bg:#e1e2e7,spinner:#e1e2e7,hl:#e1e2e7"\
+" --color=bg+:#eff1f5,bg:#eff1f5,spinner:#eff1f5,hl:#febd69"\
 " --color=fg:#414868,header:#2e7de9,info:#2e7de9,pointer:#f44747"\
-" --color=marker:#f44747,fg+:#f44747,prompt:#2e7de9,hl+:#f44747"
+" --color=marker:#2e7de9,fg+:#2e7de9,prompt:#2e7de9,hl+:#f44747"
