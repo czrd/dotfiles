e91cc46dd07c9548554ee659cd4c45fd511e0171
Takeshi Itami <tksh.itm@gmail.com>
2023-02-23 17:14:51 +0900

    modify LspReference highlight

--- nvim/.config/nvim/lua/config/color.lua
+++ nvim/.config/nvim/lua/config/color.lua
@@ -2,9 +2,6 @@ require("catppuccin").setup {
   flavour = "latte",
   term_colors = true,
   custom_highlights = {
-    LspReferenceRead = { fg = "#ffffff", bg = "#d7005f" },
-    LspReferenceWrite = { link = "LspReferenceRead" },
-    LspReferenceText = { link = "LspReferenceRead" },
     NormalFloat = { link = "Normal" },
     PmenuSel = { fg = "#ffffff", bg = "#3760bf" },
     Search = { fg = "#ffffff", bg = "#febd69" },
