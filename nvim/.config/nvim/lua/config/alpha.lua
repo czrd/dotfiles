79b1e437b0957f95b576c60c886a02b4e48e8f97
Takeshi Itami <tksh.itm@gmail.com>
2023-05-04 15:21:14 +0900

    fix alpha.nvim icons

--- nvim/.config/nvim/lua/config/alpha.lua
+++ nvim/.config/nvim/lua/config/alpha.lua
@@ -1,4 +1,6 @@
 local theta = require "alpha.themes.theta"
+local dashboard = require "alpha.themes.dashboard"
+
 theta.header.val = {
   [[                                                    ]],
   [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
@@ -10,5 +12,15 @@ theta.header.val = {
   [[                                                    ]],
 }
 theta.header.opts.hl = "Title"
+theta.buttons.val = {
+  { type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+  { type = "padding", val = 1 },
+  dashboard.button("e", "  New file", "<cmd>ene<CR>"),
+  dashboard.button("SPC f f", "󰈞  Find file"),
+  dashboard.button("SPC f g", "  Live grep"),
+  dashboard.button("c", "  Configuration", "<cmd>cd ~/.config/nvim/ <CR>"),
+  dashboard.button("u", "  Update plugins", "<cmd>PackerSync<CR>"),
+  dashboard.button("q", "󰩈  Quit", "<cmd>qa<CR>"),
+}
 
 require("alpha").setup(theta.config)
