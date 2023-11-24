98003b5ad7780d01ce5edb67715e453082e32dd0
Takeshi Itami <tksh.itm@gmail.com>
2023-11-24 17:17:56 +0900

    fix waybar script

--- waybar/.config/waybar/start.sh
+++ waybar/.config/waybar/start.sh
@@ -4,6 +4,8 @@ CONFIG_FILES="$HOME/.config/waybar/config $HOME/.config/waybar/style.css"
 trap "killall waybar" EXIT
 while true; do
     waybar &
+    pid=$!
+    echo $pid
     inotifywait -e create,modify $CONFIG_FILES
-    killall waybar
+    kill $pid
 done
