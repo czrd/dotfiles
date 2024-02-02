e902479b09f8684ac4e6d7392d6cf97d06dafd03
Takeshi Itami <tksh.itm@gmail.com>
2024-02-02 19:34:47 +0900

    add alias h

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -3,6 +3,7 @@ alias g=git
 alias t=tmux
 alias docker=podman
 alias lg=lazygit
+alias h=http
 if command -v bat &> /dev/null; then
   alias cat="bat --theme \"GitHub\""
   alias catt="bat --theme \"GitHub\""
