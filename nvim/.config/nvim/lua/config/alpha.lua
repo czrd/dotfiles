27872d1a8fd2a94f7654805b8930561cd694a3d9
Takeshi Itami <tksh.itm@gmail.com>
2024-08-12 03:11:43 +0900

    change startup screen

--- nvim/.config/nvim/lua/config/alpha.lua
+++ nvim/.config/nvim/lua/config/alpha.lua
@@ -1,32 +1,33 @@
 local theta = require "alpha.themes.theta"
 local dashboard = require "alpha.themes.dashboard"
 
-theta.header.val = {
-  [[⠀⠀⠀⣠⣾⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⣾⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-  [[⠀⣠⣾⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣤⣤⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-  [[⣼⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-  [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⣀⣀⠀⣀⣤⣤⣄⣀⠀⠀⠀⠀⠀⣀⣠⣤⣄⣀⠀⠀⠀⠀⠀⣀⣠⣤⣤⣄⡀⠀⢠⣤⣤⣤⠀⠀⠀⠀⢠⣤⣤⣤⣤⣤⣤⣤⠀⣤⣤⣄⣤⣴⣶⣦⣄⢀⣠⣴⣶⣶⣤⣄⠀]],
-  [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⣿⣿⣾⠿⠛⠛⢿⣿⣧⠀⠀⣴⣿⡿⠛⠛⠛⠿⣷⡄⠀⣰⣿⣿⠟⠛⠛⠿⣿⣷⡜⣿⣿⣿⡆⠀⠀⢀⣿⣿⣿⡟⣿⣿⣿⣿⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷]],
-  [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀⠀⠀⢻⣿⡆⣼⣿⣏⣀⣀⣀⣀⣀⣻⣿⢰⣿⡟⠁⠀⠀⠀⠀⠘⣿⣿⡼⣿⣿⣿⡀⠀⣾⣿⣿⡿⠁⣿⣿⣿⣿⠀⣿⣿⣿⠁⠀⠈⣿⣿⣿⣿⠁⠀⢹⣿⣿⣿]],
-  [[⣿⣿⣿⣿⣿⣿⠙⢿⣿⣿⣿⣿⣿⣿⣦⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⢸⣿⡇⣿⣿⠛⠛⠛⠛⠛⠛⠛⠛⢸⣿⡇⠀⠀⠀⠀⠀⠀⢹⣿⡇⠹⣿⣿⣧⣸⣿⣿⣿⠁⠀⣿⣿⣿⣿⠀⣿⣿⣿⠀⠀⠀⣿⣿⣿⣿⠀⠀⢸⣿⣿⣿]],
-  [[⣿⣿⣿⣿⣿⣿⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⢸⣿⡇⢻⣿⣆⠀⠀⠀⠀⠀⠀⠀⢸⣿⣇⠀⠀⠀⠀⠀⠀⣾⣿⠇⠀⣿⣿⣿⣿⣿⣿⠃⠀⠀⣿⣿⣿⣿⠀⣿⣿⣿⠀⠀⠀⣿⣿⣿⣿⠀⠀⢸⣿⣿⣿]],
-  [[⣿⣿⣿⣿⣿⣿⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⢸⣿⡇⠈⢿⣿⣦⣄⣀⣀⣠⣶⣶⠀⢻⣿⣦⣄⣀⣀⣠⣾⣿⠟⠀⠀⠘⣿⣿⣿⣿⠏⠀⠀⠀⣿⣿⣿⣿⠀⣿⣿⣿⠀⠀⠀⣿⣿⣿⣿⠀⠀⢸⣿⣿⣿]],
-  [[⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠛⠛⠀⠀⠀⠀⠀⠘⠛⠃⠀⠀⠉⠛⠻⠿⠿⠛⠋⠁⠀⠀⠉⠛⠻⠿⠿⠟⠋⠁⠀⠀⠀⠀⠙⠛⠛⠛⠀⠀⠀⠀⠛⠛⠛⠛⠀⠛⠛⠛⠀⠀⠀⠛⠛⠛⠛⠀⠀⠘⠛⠛⠛]],
-  [[⢿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-  [[⠀⠙⢿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-  [[⠀⠀⠀⠙⢿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+require("alpha").setup {
+  layout = {
+    { type = "padding", val = 3 },
+    {
+      type = "text",
+      val = "────────── Recent files ──────────",
+      opts = { hl = "Conceal", position = "center" },
+    },
+    { type = "padding", val = 1 },
+    {
+      type = "group",
+      val = function()
+        return { theta.mru(0, vim.fn.getcwd()) }
+      end,
+      opts = { shrink_margin = false },
+    },
+    { type = "padding", val = 3 },
+    {
+      type = "text",
+      val = "────────── Quick links ──────────",
+      opts = { hl = "Conceal", position = "center" },
+    },
+    { type = "padding", val = 1 },
+    dashboard.button("e", "  New file", "<cmd>ene<CR>"),
+    dashboard.button("SPC f f", "󰈞  Find file"),
+    dashboard.button("SPC f g", "  Live grep"),
+    dashboard.button("u", "  Update plugins", "<cmd>Lazy update<CR>"),
+    dashboard.button("q", "󰩈  Quit", "<cmd>qa<CR>"),
+  },
 }
-
-theta.header.opts.hl = "Comment"
-theta.buttons.val = {
-  { type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
-  { type = "padding", val = 1 },
-  dashboard.button("e", "  New file", "<cmd>ene<CR>"),
-  dashboard.button("SPC f f", "󰈞  Find file"),
-  dashboard.button("SPC f g", "  Live grep"),
-  dashboard.button("c", "  Configuration", "<cmd>cd ~/.config/nvim/ <CR>"),
-  dashboard.button("u", "  Update plugins", "<cmd>Lazy update<CR>"),
-  dashboard.button("q", "󰩈  Quit", "<cmd>qa<CR>"),
-}
-
-require("alpha").setup(theta.config)
