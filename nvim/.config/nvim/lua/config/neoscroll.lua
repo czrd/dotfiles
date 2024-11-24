804f77c593ef9d1ba7a899cf9d4cee731cad7728
Takeshi Itami <tksh.itm@gmail.com>
2024-11-25 00:17:32 +0900

    fix neoscroll.lua

--- nvim/.config/nvim/lua/config/neoscroll.lua
+++ nvim/.config/nvim/lua/config/neoscroll.lua
@@ -11,12 +11,37 @@ neoscroll.setup {
   hide_cursor = false,
 }
 
-require("neoscroll.config").set_mappings {
-  ["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "200" } },
-  ["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "200" } },
-  ["<C-b>"] = { "scroll", { "-vim.wo.scroll", "true", "200" } },
-  ["<C-f>"] = { "scroll", { "vim.wo.scroll", "true", "200" } },
-  ["zt"] = { "zt", { "100" } },
-  ["zz"] = { "zz", { "100" } },
-  ["zb"] = { "zb", { "100" } },
+local keymap = {
+  ["<C-u>"] = function()
+    neoscroll.ctrl_u { duration = 200 }
+  end,
+  ["<C-d>"] = function()
+    neoscroll.ctrl_d { duration = 200 }
+  end,
+  ["<C-b>"] = function()
+    neoscroll.ctrl_b { duration = 200 }
+  end,
+  ["<C-f>"] = function()
+    neoscroll.ctrl_f { duration = 200 }
+  end,
+  ["<C-y>"] = function()
+    neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 })
+  end,
+  ["<C-e>"] = function()
+    neoscroll.scroll(0.1, { move_cursor = false, duration = 100 })
+  end,
+  ["zt"] = function()
+    neoscroll.zt { half_win_duration = 200 }
+  end,
+  ["zz"] = function()
+    neoscroll.zz { half_win_duration = 200 }
+  end,
+  ["zb"] = function()
+    neoscroll.zb { half_win_duration = 200 }
+  end,
 }
+
+local modes = { "n", "v", "x" }
+for key, func in pairs(keymap) do
+  vim.keymap.set(modes, key, func)
+end
