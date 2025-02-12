784159f5b7d42e4fca237df66c001e189bbd06b2
Takeshi Itami <tksh.itm@gmail.com>
2025-02-13 00:50:15 +0900

    change fzf accent color

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -1,4 +1,4 @@
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
 " --color=bg+:#eff1f5,bg:#eff1f5,spinner:#eff1f5,hl:#df8e1d"\
-" --color=fg:#4c4f69,header:#d20f39,info:#d20f39,pointer:#d20f39"\
-" --color=marker:#d20f39,fg+:#d20f39,prompt:#d20f39,hl+:#df8e1d"
+" --color=fg:#4c4f69,header:#4CAF50,info:#4CAF50,pointer:#4CAF50"\
+" --color=marker:#4CAF50,fg+:#4CAF50,prompt:#4CAF50,hl+:#df8e1d"
