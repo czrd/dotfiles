f926263bf39a297a8f943cf55c2e72e90acc3f5d
Takeshi Itami <tksh.itm@gmail.com>
2025-07-28 21:54:05 +0900

    change lualine theme

--- nvim/.config/nvim/lua/config/lualine.lua
+++ nvim/.config/nvim/lua/config/lualine.lua
@@ -7,44 +7,61 @@ end
 local catppuccin = require "catppuccin.utils.lualine" "latte"
 local palette = require("catppuccin.palettes").get_palette "latte"
 
-local main_section = { fg = palette.base, bg = palette.lavender, gui = "bold" }
-local sub_section = { fg = palette.text, bg = palette.crust }
+local main1_section = { fg = palette.base, bg = palette.lavender, gui = "bold" }
+local main2_section = { fg = palette.text, bg = palette.crust }
 local hidden_section = { fg = palette.base, bg = palette.base }
+local sub1_section = { fg = palette.text, bg = palette.surface2 }
+local sub2_section = { fg = palette.text, bg = palette.surface1 }
+local sub3_section = { fg = palette.text, bg = palette.surface0 }
 
-catppuccin.normal.a = main_section
-catppuccin.normal.b = sub_section
+catppuccin.normal.a = main1_section
+catppuccin.normal.b = main2_section
 catppuccin.normal.c = hidden_section
-catppuccin.normal.z = sub_section
+catppuccin.normal.x = sub1_section
+catppuccin.normal.y = sub2_section
+catppuccin.normal.z = sub3_section
 
-catppuccin.command.a = main_section
-catppuccin.command.b = sub_section
+catppuccin.command.a = main1_section
+catppuccin.command.b = main2_section
 catppuccin.command.c = hidden_section
-catppuccin.command.z = sub_section
+catppuccin.command.x = sub1_section
+catppuccin.command.y = sub2_section
+catppuccin.command.z = sub3_section
 
-catppuccin.terminal.a = main_section
-catppuccin.terminal.b = sub_section
+catppuccin.terminal.a = main1_section
+catppuccin.terminal.b = main2_section
 catppuccin.terminal.c = hidden_section
-catppuccin.terminal.z = sub_section
+catppuccin.terminal.x = sub1_section
+catppuccin.terminal.y = sub2_section
+catppuccin.terminal.z = sub3_section
 
-catppuccin.insert.a = main_section
-catppuccin.insert.b = sub_section
+catppuccin.insert.a = main1_section
+catppuccin.insert.b = main2_section
 catppuccin.insert.c = hidden_section
-catppuccin.insert.z = sub_section
+catppuccin.insert.x = sub1_section
+catppuccin.insert.y = sub2_section
+catppuccin.insert.z = sub3_section
 
-catppuccin.visual.a = main_section
-catppuccin.visual.b = sub_section
+catppuccin.visual.a = main1_section
+catppuccin.visual.b = main2_section
 catppuccin.visual.c = hidden_section
-catppuccin.visual.z = sub_section
+catppuccin.visual.x = sub1_section
+catppuccin.visual.y = sub2_section
+catppuccin.visual.z = sub3_section
 
-catppuccin.replace.a = main_section
-catppuccin.replace.b = sub_section
+catppuccin.replace.a = main1_section
+catppuccin.replace.b = main2_section
 catppuccin.replace.c = hidden_section
-catppuccin.replace.z = sub_section
+catppuccin.replace.x = sub1_section
+catppuccin.replace.y = sub2_section
+catppuccin.replace.z = sub3_section
 
-catppuccin.inactive.a = main_section
-catppuccin.inactive.b = sub_section
+catppuccin.inactive.a = main1_section
+catppuccin.inactive.b = main2_section
 catppuccin.inactive.c = hidden_section
-catppuccin.inactive.z = sub_section
+catppuccin.inactive.x = sub1_section
+catppuccin.inactive.y = sub2_section
+catppuccin.inactive.z = sub3_section
 
 ll.setup {
   sections = {
@@ -53,17 +70,20 @@ ll.setup {
     },
     lualine_b = {
       "branch",
+      "filename",
       "diff",
       { "diagnostics", symbols = { error = " ", warn = " ", info = " ", hint = " " } },
-      "filename",
     },
     lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
+    lualine_x = {
+      { "encoding", separator = { left = "", right = "" }, right_padding = 3 },
+    },
+    lualine_y = {
+      { "progress", separator = { left = "", right = "" }, right_padding = 3 },
+    },
     lualine_z = {
-      "encoding",
-      "progress",
-      "filetype",
+      { "filetype", separator = { left = "", right = "" } },
+      { "lsp_status", separator = { left = "", right = "" } },
     },
   },
   options = {
