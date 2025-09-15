0461da619aa31d0ed22b85e868178611826347aa
Takeshi Itami <tksh.itm@gmail.com>
2025-09-15 21:39:39 +0900

    edit wezterm.lua

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 local config = wezterm.config_builder()
 
 config.font_size = 15
@@ -7,5 +8,11 @@ config.font = wezterm.font("JetBrains Mono")
 config.enable_tab_bar = false
 config.enable_wayland = true
 config.scrollback_lines = 100000
+config.window_close_confirmation = "NeverPrompt"
+
+wezterm.on("gui-startup", function(cmd)
+	local _, _, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 
 return config
