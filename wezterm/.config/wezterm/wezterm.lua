30d5b005377f1e342f32ef6442f60db3f305eadf
Takeshi Itami <tksh.itm@gmail.com>
2025-11-02 23:26:39 +0900

    add wezterm window_frame config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,6 +9,18 @@ config.enable_tab_bar = false
 config.enable_wayland = true
 config.scrollback_lines = 100000
 config.window_close_confirmation = "NeverPrompt"
+config.window_frame = {
+	inactive_titlebar_bg = "#eff1f5",
+	active_titlebar_bg = "#eff1f5",
+	inactive_titlebar_fg = "#eff1f5",
+	active_titlebar_fg = "#eff1f5",
+	inactive_titlebar_border_bottom = "#eff1f5",
+	active_titlebar_border_bottom = "#eff1f5",
+	button_fg = "#acb0be",
+	button_bg = "#eff1f5",
+	button_hover_fg = "#acb0be",
+	button_hover_bg = "#eff1f5",
+}
 
 wezterm.on("gui-startup", function(cmd)
 	local _, _, window = mux.spawn_window(cmd or {})
