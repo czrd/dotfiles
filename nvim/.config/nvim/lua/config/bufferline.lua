049a83c063468e3ab81617fed504affe464ba50f
Takeshi Itami <tksh.itm@gmail.com>
2025-11-23 16:57:06 +0900

    edit bufferline config

--- nvim/.config/nvim/lua/config/bufferline.lua
+++ nvim/.config/nvim/lua/config/bufferline.lua
@@ -9,13 +9,28 @@ bl.setup {
     show_buffer_close_icons = false,
     show_close_icon = false,
     diagnostics = "nvim_lsp",
+    indicator = {
+      style = "none",
+    },
   },
   highlights = {
     indicator_selected = {
-      fg = "#59795F",
+      fg = "#eff1f5",
+    },
+    indicator_visible = {
+      fg = "#eff1f5",
+    },
+    separator = {
+      fg = "#eff1f5",
+    },
+    separator_selected = {
+      fg = "#eff1f5",
+    },
+    separator_visible = {
+      fg = "#eff1f5",
     },
     fill = {
-      bg = '#eff1f5'
+      bg = "#eff1f5",
     },
   },
 }
