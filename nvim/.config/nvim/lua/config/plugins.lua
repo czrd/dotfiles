748c183027e3de3ea486dc32677ef7c5007440b3
Takeshi Itami <tksh.itm@gmail.com>
2025-12-17 22:12:30 +0900

    delete metals.nvim

--- nvim/.config/nvim/lua/config/plugins.lua
+++ nvim/.config/nvim/lua/config/plugins.lua
@@ -160,52 +160,6 @@ require("lazy").setup({
     end,
   },
 
-  -- Scala support
-  {
-    "scalameta/nvim-metals",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    ft = { "scala", "sbt", "java" },
-    opts = function()
-      local metals_config = require("metals").bare_config()
-      metals_config.on_attach = function(client, bufnr)
-        -- your on_attach function
-        if client.server_capabilities.documentHighlightProvider then
-          vim.api.nvim_create_augroup("lsp_document_highlight", {
-            clear = false,
-          })
-          vim.api.nvim_clear_autocmds {
-            buffer = bufnr,
-            group = "lsp_document_highlight",
-          }
-          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-            buffer = bufnr,
-            group = "lsp_document_highlight",
-            callback = vim.lsp.buf.document_highlight,
-          })
-          vim.api.nvim_create_autocmd("CursorMoved", {
-            buffer = bufnr,
-            group = "lsp_document_highlight",
-            callback = vim.lsp.buf.clear_references,
-          })
-        end
-      end
-
-      return metals_config
-    end,
-    config = function(self, metals_config)
-      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = self.ft,
-        callback = function()
-          require("metals").initialize_or_attach(metals_config)
-        end,
-        group = nvim_metals_group,
-      })
-    end,
-  },
-
   -- utils
   "lewis6991/impatient.nvim",
   "nvim-lua/plenary.nvim",
