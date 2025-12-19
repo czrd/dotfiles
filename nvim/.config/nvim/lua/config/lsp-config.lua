d7021abe04e43fb0e2affdcaaba319b2114826d3
Takeshi Itami <tksh.itm@gmail.com>
2025-12-19 20:38:19 +0900

    configure lua_ls

--- nvim/.config/nvim/lua/config/lsp-config.lua
+++ nvim/.config/nvim/lua/config/lsp-config.lua
@@ -28,10 +28,7 @@ local servers = {
     "lua_ls",
     settings = {
       Lua = {
-        runtime = { version = "LuaJIT" },
-        diagnostics = { globals = { "vim" } },
         workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-        telemetry = { enable = false },
       },
     },
   },
