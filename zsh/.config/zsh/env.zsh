dd46afab2427524d90b286a562f675db83052310
Takeshi Itami <tksh.itm@gmail.com>
2026-04-30 22:41:14 +0900

    format code

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -8,8 +8,8 @@ eval "$(pyenv init --path)"
 
 # rust
 # set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/.local/bin" ]; then
-  PATH="$HOME/.local/bin:$PATH"
+if [ -d "$HOME/.cargo/bin" ]; then
+  PATH="$HOME/.cargo/bin:$PATH"
 fi
 
 # >>> coursier install directory >>>
