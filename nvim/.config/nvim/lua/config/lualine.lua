ee41a2ce5a69457242c1a1bebe158b520b991306
Takeshi Itami <tksh.itm@gmail.com>
2026-07-19 13:35:07 +0900

    fix lualine

--- nvim/.config/nvim/lua/config/lualine.lua
+++ nvim/.config/nvim/lua/config/lualine.lua
@@ -4,64 +4,26 @@ if not ok then
   return
 end
 
-local theme = require "catppuccin.utils.lualine" "latte"
-local palette = require("catppuccin.palettes").get_palette "latte"
+local palette = require("nightfox.palette").load "dawnfox"
 
-local main1_section = { fg = palette.base, bg = palette.green, gui = "bold" }
-local main2_section = { fg = palette.text, bg = palette.crust }
-local sub1_section = { fg = palette.text, bg = palette.crust }
-local sub2_section = { fg = palette.text, bg = palette.crust }
-local sub3_section = { fg = palette.text, bg = palette.crust }
-local hidden_section = { fg = palette.base, bg = palette.base }
+local base = {
+  a = { fg = palette.bg1, bg = palette.green.bright, gui = "bold" },
+  b = { fg = palette.fg1, bg = palette.bg2 },
+  c = { fg = palette.bg1, bg = palette.bg1 },
+  x = { fg = palette.fg1, bg = palette.bg2 },
+  y = { fg = palette.fg1, bg = palette.bg2 },
+  z = { fg = palette.fg1, bg = palette.bg2 },
+}
 
-theme.normal.a = main1_section
-theme.normal.b = main2_section
-theme.normal.c = hidden_section
-theme.normal.x = sub1_section
-theme.normal.y = sub2_section
-theme.normal.z = sub3_section
-
-theme.command.a = main1_section
-theme.command.b = main2_section
-theme.command.c = hidden_section
-theme.command.x = sub1_section
-theme.command.y = sub2_section
-theme.command.z = sub3_section
-
-theme.terminal.a = main1_section
-theme.terminal.b = main2_section
-theme.terminal.c = hidden_section
-theme.terminal.x = sub1_section
-theme.terminal.y = sub2_section
-theme.terminal.z = sub3_section
-
-theme.insert.a = main1_section
-theme.insert.b = main2_section
-theme.insert.c = hidden_section
-theme.insert.x = sub1_section
-theme.insert.y = sub2_section
-theme.insert.z = sub3_section
-
-theme.visual.a = main1_section
-theme.visual.b = main2_section
-theme.visual.c = hidden_section
-theme.visual.x = sub1_section
-theme.visual.y = sub2_section
-theme.visual.z = sub3_section
-
-theme.replace.a = main1_section
-theme.replace.b = main2_section
-theme.replace.c = hidden_section
-theme.replace.x = sub1_section
-theme.replace.y = sub2_section
-theme.replace.z = sub3_section
-
-theme.inactive.a = main1_section
-theme.inactive.b = main2_section
-theme.inactive.c = hidden_section
-theme.inactive.x = sub1_section
-theme.inactive.y = sub2_section
-theme.inactive.z = sub3_section
+local theme = {
+  normal = base,
+  command = base,
+  terminal = base,
+  intert = base,
+  visual = base,
+  replace = base,
+  inactive = base,
+}
 
 ll.setup {
   sections = {
